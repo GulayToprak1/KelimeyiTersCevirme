5e9ecf576990fc5585fe9a716873f2e3ede60b98
casper <casper@GLAYTOPRAK>
2023-12-03 16:48:25 +0300

    Add project files.

---
+++ KelimeyiTersCevirme/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KelimeyiTersCevirme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ters Çevirmek İstediğiniz Kelimeyi Giriniz:");
+            string kelime = Console.ReadLine();
+
+            string terskelime = "";
+            for (int i = kelime.Length - 1; i >= 0; i--)
+            {
+                terskelime += kelime[i];
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Kelimenin Tersi:" + terskelime);
+
+            Console.ReadLine();
+        }
+    }
+}
